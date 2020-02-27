f13a72a2cc295dce9c348d074064fc2c036114f1
Henrique Silva <henrique.silva@hbsis.com.br>
2020-02-27 16:37:13 -0300

    Erro na autenticação do mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://henrique:<henrique>@henrique-silva-tc8zq.gcp.mongodb.net/wazedev?retryWrites=true&w=majority', {
-    useNewUrlParser: true, 
+mongoose.connect(
+  "mongodb+srv://henrique:<henrique>@henrique-silva-tc8zq.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
-app.get('/', (req, res) => {
-    return res.json({
-        message: 'teste'
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);

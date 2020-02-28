f8f26fd46b2f1a631093911d26bf69405afdd39e
Henrique Silva <henrique.silva@hbsis.com.br>
2020-02-28 17:11:40 -0300

    Continuando o projeto web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(
-  "mongodb+srv://henrique:henrique@henrique-silva-tc8zq.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  'mongodb+srv://henrique:henrique@henrique-silva-tc8zq.gcp.mongodb.net/test?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 );
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);

0d3c40eccd48d237c439f467749849481bd3b05e
Henrique Silva <henriwasd@gmail.com>
2020-03-03 19:07:46 -0300

    Finalizando o WEB

---
+++ web/src/components/DevItem/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './styles.css';
+
+function DevItem({ dev }) {
+    return (
+        <li className='dev-item'>
+            <header>
+                <img
+                    src={dev.avatar_url}
+                    alt='avatar'
+                />
+                <div className='user-info'>
+                    <strong>{dev.name}</strong>
+                    <span>ReactJs, VueJs</span>
+                </div>
+            </header>
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+        </li>
+    )
+}
+
+export default DevItem;
